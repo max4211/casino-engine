f3b0817796dc3b98bd1ab48d01b79c85e5046c2b
Eric Doppelt <ead45@duke.edu>
2020-04-07 00:58:34 -0400

    created a Formatter that currently operates on VBOxes

--- src/Formatting/Formatter.java
+++ src/Formatting/Formatter.java
@@ -1,12 +1,19 @@
 package Formatting;
 
+import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 
 public class Formatter {
 
     public Formatter() {}
 
-    public void formatVBox(VBox rawVBox) {
+    public void formatVBox(VBox rawVBox, double height, double width) {
+        rawVBox.setMinHeight(height);
+        rawVBox.setMaxHeight(height);
 
+        rawVBox.setMinWidth(width);
+        rawVBox.setMaxWidth(width);
+
+        rawVBox.setAlignment(Pos.CENTER);
     }
 }
