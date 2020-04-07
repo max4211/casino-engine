cbd77459c9db0cf5e7fbb669f044576e1dc1aa4d
max <max.smith@duke.edu>
2020-04-06 23:14:04 -0400

    added shells for evaluator package methods and interfaces

---
+++ src/engine/evaluator/BetEvaluatorInterface.java
@@ -0,0 +1,21 @@
+package engine.evaluator;
+
+import java.util.Collection;
+
+public interface BetEvaluatorInterface {
+
+    /**
+     * Iterates over all of the bets in the collection parameter and assigns each one a winning or losing value stores in a boolean instance variable
+     * Multiple bets can be winners given the nature of a push
+     * Calls the HandEvaluator compare method often to determine the winner of two hands
+     * @param Bets is a collection of all bets to iterate over from a turn to assign winning and losing values
+     */
+    public void evaluateBets(Collection Bets);
+
+    /**
+     * Updates the wager of a bet given its payout odds, such that bets with large payout odds become worth more value
+     * The wager of the bet is then added to a player's bankroll, so this allows the cashing of the ticket to reflect the payout value
+     * @param Bets is a collection of winning bets from a turn to iterate over and update payout odds
+     */
+    public void updateWagers(Collection Bets);
+}
