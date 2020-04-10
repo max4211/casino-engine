27664bbe9f5b527c250b62f082b78f109a2b0d44
max <max.smith@duke.edu>
2020-04-09 23:35:11 -0400

    completed hand classifier test

--- src/engine/evaluator/HandClassifierTest.java
+++ src/engine/evaluator/HandClassifierTest.java
@@ -2,6 +2,7 @@ package engine.evaluator;
 
 import engine.dealer.Card;
 import engine.hand.Hand;
+import engine.hand.HandOutcome;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,10 +17,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class HandClassifierTest {
 
     @Test
-    void testSumHandEvaluations() {
+    void testBlackJack() {
+        HandClassifier hc = createHandClassifier();
+        Hand bjTest = new Hand(List.of(new Card("Heart",  11), new Card("Diamond", 10)));
+        hc.classifyHand(bjTest);
+        assertEquals(bjTest.getClassification().getName(), "Blackjack");
+    }
+
+    @Test
+    void testBust() {
+        HandClassifier hc = createHandClassifier();
+        Hand bustTest = new Hand(List.of(new Card("Heart",  11), new Card("Diamond", 10), new Card("Spade", 11)));
+        hc.classifyHand(bustTest);
+        assertEquals(bustTest.getOutcome(), HandOutcome.LOSS);
+    }
+
+    @Test
+    void TestUnder21() {
+        HandClassifier hc = createHandClassifier();
+        Hand under22Test = new Hand(List.of(new Card("Spade", 11)));
+        hc.classifyHand(under22Test);
+        assertEquals(under22Test.getClassification().getName(), "SumUnder22");
+        assertEquals(under22Test.getClassification().getRank(), 1);
+    }
+
+    private HandClassifier createHandClassifier() {
         List<String> winners = List.of("Blackjack", "SumUnder22");
         List<String> losers = List.of("SumOver21");
-        Hand bjTest = new Hand(List.of(new Card("Heart",  11), new Card("Diamond", 10)));
-        Hand h = new Hand(List.of(new Card("Heart", 11), new Card("Heart", 10)));
+        return new HandClassifier(winners, losers);
     }
 }
