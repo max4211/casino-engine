084f326c2bb311af5ee6bbbbaee069daba456b72
Eric Doppelt <ead45@duke.edu>
2020-04-09 23:35:38 -0400

    created test package with empty HandViewTest

---
+++ tests/GameView/NodeViews/HandViewTest.java
@@ -0,0 +1,28 @@
+package GameView.NodeViews;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandViewTest {
+
+    @Test
+    void getView() {
+    }
+
+    @Test
+    void showCard() {
+    }
+
+    @Test
+    void addCard() {
+    }
+
+    @Test
+    void removeCard() {
+    }
+
+    @Test
+    void clearHand() {
+    }
+}
