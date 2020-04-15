c702bcb364d8669c10a1f6a8209b7c8ab841dd5a
max <max.smith@duke.edu>
2020-04-15 17:46:38 -0400

    implmemented priority queue sorting of bet evaluation for gruop gamesg

--- src/engine/evaluator/bet/BetEvaluator.java
+++ src/engine/evaluator/bet/BetEvaluator.java
@@ -2,11 +2,15 @@ package engine.evaluator.bet;
 
 import engine.bet.Bet;
 import engine.evaluator.handevaluator.HandEvaluator;
+import engine.hand.ClassifiedHand;
 import engine.hand.PlayerHand;
 import engine.hand.HandOutcome;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class BetEvaluator implements BetEvaluatorInterface {
 
@@ -16,37 +20,68 @@ public class BetEvaluator implements BetEvaluatorInterface {
         this.myHandEvaluator = handEvaluator;
     }
 
-    // TODO - finish implementing evaluate bets
     @Override
-    public void evaluateBets(List<Bet> bets) {
-        List<Bet> winnerCandidate = new ArrayList<>(bets);
-        boolean foundWinner = false;
-        while (!foundWinner) {
-            for (int index = 0; index < bets.size(); index ++) {
-                Bet myBet = bets.get(index);
-                boolean myWinner = true;
-                for (Bet otherBet: bets) {
-                    if (notSameBet(myBet, otherBet)) {
-                        PlayerHand myHand = myBet.getHand();
-                        PlayerHand otherHand = otherBet.getHand();
-                        int myCompare = this.myHandEvaluator.compare(myHand, otherHand);
-                        if (myCompare > 0)
-                            removeFromWinners(winnerCandidate, otherBet);
-                        else if (myCompare < 0)
-                            removeFromWinners(winnerCandidate, myBet);
-                    }
-                }
-            }
+    public void evaluateBets(@NotNull List<Bet> bets) {
+        PriorityQueue<Bet> pq = new PriorityQueue<>(bets.size(), new BetComparator());
+        pq.addAll(bets);
+        assignSortedBets(pq);
+    }
+
+    private void assignSortedBets(PriorityQueue<Bet> pq) {
+        Bet first = pq.poll();
+        List<Bet> winners = new ArrayList<Bet>(List.of(first));
+        while (traversePQ(pq, first)) {
+            winners.add(pq.poll());
+        }
+        assignWinningBets(winners);
+        assignLosingBets(pq);
+    }
+
+    private boolean traversePQ(PriorityQueue<Bet> pq, Bet first) {
+        BetComparator bc = new BetComparator();
+        boolean notEmpty = !pq.isEmpty();
+        boolean stillEqual = bc.compare(first, pq.peek()) == 0;
+        return notEmpty && stillEqual;
+    }
+
+    private void assignWinningBets(List<Bet> winners) {
+        HandOutcome outcome = HandOutcome.WIN;
+        if (winners.size() > 1)
+            outcome = HandOutcome.TIE;
+        for (Bet b: winners)
+            assignOutcome(b.getHand(), outcome);
+    }
+
+    private void assignLosingBets(PriorityQueue<Bet> pq) {
+        while (!(pq.isEmpty()))
+            assignLoser(pq.poll().getHand());
+    }
+
+    class BetComparator implements Comparator<Bet>{
+
+        @Override
+        public int compare(Bet b1, Bet b2) {
+            ClassifiedHand c1 = b1.getHand().getClassification();
+            ClassifiedHand c2 = b2.getHand().getClassification();
+            int rankDiff = c1.getRank() - c2.getRank();
+            double powDiff = c1.getPower() - c2.getPower();
+
+            if (rankDiff != 0)
+                return rankDiff;
+            else
+                return doubleToInt(powDiff);
+        }
+
+        private int doubleToInt(double d) {
+            if (d < 0)
+                return -1;
+            else if (d > 0)
+                return 1;
+            else
+                return 0;
         }
     }
 
-    private void removeFromWinners(List<Bet> winnerCandidate, Bet bet) {
-        try {
-            winnerCandidate.remove(bet);
-        } catch (Exception e) {
-            System.out.println("already removed bet from list");
-        }
-    }
 
     @Override
     public void updateWagers(List<Bet> bets) {
@@ -70,18 +105,30 @@ public class BetEvaluator implements BetEvaluatorInterface {
     }
 
     private void compareOverZero(PlayerHand h1, PlayerHand h2) {
-        assignOutcome(h1, HandOutcome.WIN);
-        assignOutcome(h2, HandOutcome.LOSS);
+        assignWinner(h1);
+        assignLoser(h2);
     }
 
     private void compareUnderZero(PlayerHand h1, PlayerHand h2) {
-        assignOutcome(h1, HandOutcome.LOSS);
-        assignOutcome(h2, HandOutcome.WIN);
+        assignLoser(h1);
+        assignWinner(h2);
     }
 
     private void compareEqualsZero(PlayerHand h1, PlayerHand h2) {
-        assignOutcome(h1, HandOutcome.TIE);
-        assignOutcome(h2, HandOutcome.TIE);
+        assignTie(h1);
+        assignTie(h2);
+    }
+
+    private void assignLoser(PlayerHand h) {
+        assignOutcome(h, HandOutcome.LOSS);
+    }
+
+    private void assignWinner(PlayerHand h) {
+        assignOutcome(h, HandOutcome.WIN);
+    }
+
+    private void assignTie(PlayerHand h) {
+        assignOutcome(h, HandOutcome.TIE);
     }
 
     private void assignOutcome(PlayerHand playerHand, HandOutcome outcome) {
