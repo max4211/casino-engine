c5b4f59703788f16042aa69332752d905b19b65f
max <max.smith@duke.edu>
2020-04-23 14:31:52 -0400

    refactored lobby reader file input to a list of files

--- src/UI/LobbyView/GameIcon.java
+++ src/UI/LobbyView/GameIcon.java
@@ -7,23 +7,23 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-public abstract class GameIcon implements NodeViewInterface {
+public class GameIcon implements NodeViewInterface {
 
     private VBox myGameIcon;
     private Formatter myFormatter;
+    private List<File> myFiles;
 
     //TODO: duplicated in CardView
-
     private static final String BLACKJACK = "Blackjack";
     private static final String BLACKJACK_ICON_ID = "blackJack-icon";
     private static final String CUSTOM = "Custom Game";
     private static final String CUSTOM_ICON_ID = "custom-icon";
 
-    public GameIcon(String imageFile, String gameName, Consumer<Map<String, File>> myFileAccepter) {
-
+    public GameIcon(String imageFile, String gameName, List<File> files) {
         myGameIcon = new VBox();
         //TODO: move this into formatter
         myGameIcon.setAlignment(Pos.CENTER);
@@ -40,8 +40,11 @@ public abstract class GameIcon implements NodeViewInterface {
 
         myFormatter = new Formatter();
         myFormatter.formatGameIconView(myIconButton.getView());
-        myGameIcon.setOnMouseClicked(e -> {
-            myFileAccepter.accept(getFiles());
+
+        this.myFiles = files;
+        this.myGameIcon.setOnMouseClicked(e -> {
+            // TODO - set mouse clicked event (route to validator
+//            myFileAccepter.accept(getFiles());
         });
 
         //TODO: move this to formatting
@@ -54,5 +57,7 @@ public abstract class GameIcon implements NodeViewInterface {
         return myGameIcon;
     }
 
-    protected abstract Map<String, File> getFiles();
+    public List<File> getFiles() {
+        return this.myFiles;
+    }
 }
