c282c122e0958acad46bdd5d832216af699bb9a4
Eric Doppelt <ead45@duke.edu>
2020-04-23 13:12:48 -0400

    fixed web browser and changed to render method

--- src/UI/WebView/Browser.java
+++ src/UI/WebView/Browser.java
@@ -3,15 +3,15 @@ package UI.WebView;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-
-import java.io.File;
+import javafx.stage.Stage;
 
 public class Browser implements BrowserInterface {
 
     private static final String TEST_PAGE = "http://www.casino308.com";
+    private static final int BROWSER_SIZE = 600;
 
     private WebView myWebView;
     private WebEngine myWebEngine;
@@ -24,11 +24,13 @@ public class Browser implements BrowserInterface {
     }
 
     @Override
-    public Scene getScene(int width, int height) {
-        BorderPane root = new BorderPane();
-        root.setCenter(makePageDisplay());
-        Scene scene = new Scene(root, width, height);
-        return scene;
+    public void render() {
+        Pane root = new Pane();
+        root.getChildren().add(makePageDisplay());
+        Scene scene = new Scene(root, BROWSER_SIZE, BROWSER_SIZE);
+        Stage renderedStage = new Stage();
+        renderedStage.setScene(scene);
+        renderedStage.show();
     }
 
     private Node makePageDisplay() {
--- src/UI/WebView/BrowserInterface.java
+++ src/UI/WebView/BrowserInterface.java
@@ -1,8 +1,6 @@
 package UI.WebView;
 
-import javafx.scene.Scene;
-
 public interface BrowserInterface {
 
-    Scene getScene(int width, int height);
+    void render();
 }
