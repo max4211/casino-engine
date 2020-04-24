cda556cc51be83253f835684379d648cc924097f
Eric Doppelt <ead45@duke.edu>
2020-04-24 17:50:10 -0400

    made height multipliers static

--- src/UI/Utilities/ScreenPosition.java
+++ src/UI/Utilities/ScreenPosition.java
@@ -10,7 +10,6 @@ public enum ScreenPosition {
     MIDDLE ('M'),
     RIGHT ('R');
 
-    // TODO: why will it let me access some but not all?
     private final Rectangle2D SCREEN_BOUNDS = Screen.getPrimary().getBounds();
     private final double SCREEN_WIDTH = SCREEN_BOUNDS.getWidth();
     private final double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
@@ -19,9 +18,9 @@ public enum ScreenPosition {
     private static final double MIDDLE_WIDTH_MULTIPLIER = .5;
     private static final double RIGHT_WIDTH_MULTIPLIER = .68;
 
-    private final double LEFT_HEIGHT_MULTIPLIER = .25;
-    private final double MIDDLE_HEIGHT_MULTIPLIER = .20;
-    private final double RIGHT_HEIGHT_MULTIPLIER = .25;
+    private static final double LEFT_HEIGHT_MULTIPLIER = .25;
+    private static final double MIDDLE_HEIGHT_MULTIPLIER = .20;
+    private static final double RIGHT_HEIGHT_MULTIPLIER = .25;
 
 
     private double x;
