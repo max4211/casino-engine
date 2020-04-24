c8e1c3efc89bc23cc111c34f4e11559d0806bc8a
Eric Doppelt <ead45@duke.edu>
2020-04-24 03:21:45 -0400

    updated lobbyview constructor to prepare for data drive in the morning

--- src/UI/LobbyView/LobbyView.java
+++ src/UI/LobbyView/LobbyView.java
@@ -3,34 +3,53 @@ package UI.LobbyView;
 import UI.ExceptionHandling.ExceptionDisplayer;
 import UI.Interfaces.NodeViewInterface;
 import UI.LanguageBundle;
+import UI.Settings.SettingsBar;
+import javafx.geometry.Pos;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
 import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 public class LobbyView implements NodeViewInterface {
 
+    VBox myVBox;
     FlowPane myFlowPane;
+
+    private static final String PATH_TO_ICONS = "resources/iconImages/lobbyViewIcons/";
+    private static final String PATH_TO_LANGUAGE = "/languages/";
+    private static final String PATH_TO_CSS = "/styleSheets";
+
+    private static final String INFO_TAG = "Info";
+    private static final String ERROR_TAG = "Error";
 
     private static final String NAME_TAG = "Name";
     private static final String ICON_TAG = "Icon";
 
-    private static final String FLOWPANE_CSS_ID = "full-lobby";
+    private static final String CSS_ID = "lobby-view";
     private static final int DEFAULT_CSS_INDEX = 0;
     private static final int DEFAULT_LANGUAGE_INDEX = 0;
-
+    private static final int VBOX_SPACING = 5;
     private LanguageBundle myLanguageBundle;
     private ExceptionDisplayer myExceptionDisplayer;
 
-    public LobbyView(List<String> styleSheets, List<String> languages, String errorIcon, String errorCSS, List<Map<String, String>> generalInfo, List<List<File>> files) {
-        myFlowPane = new FlowPane();
-        myFlowPane.setId(FLOWPANE_CSS_ID);
-        myFlowPane.getStylesheets().add(styleSheets.get(DEFAULT_CSS_INDEX));
+    public LobbyView(List<String> styleSheets, List<String> languages, String iconProperties, String errorCSS, List<Map<String, String>> generalInfo, List<List<File>> files) {
+        myVBox = new VBox();
+        myVBox.setId(CSS_ID);
+        myVBox.setSpacing(VBOX_SPACING);
+        myVBox.getStylesheets().add(styleSheets.get(DEFAULT_CSS_INDEX));
 
+        ResourceBundle myIconResources = ResourceBundle.getBundle(iconProperties);
         myLanguageBundle = new LanguageBundle(languages.get(DEFAULT_LANGUAGE_INDEX));
-        myExceptionDisplayer = new ExceptionDisplayer(errorIcon, errorCSS, myLanguageBundle);
+        System.out.println(myIconResources.getString(ERROR_TAG));
+        myExceptionDisplayer = new ExceptionDisplayer(myIconResources.getString(ERROR_TAG), errorCSS, myLanguageBundle);
+        SettingsBar addedSettings = new SettingsBar(e -> updateCSS(e), styleSheets, e -> updateLanguage(e), languages, myIconResources.getString(INFO_TAG));
+        myVBox.getChildren().add(addedSettings.getView());
 
+        myFlowPane = new FlowPane();
+        myFlowPane.setAlignment(Pos.CENTER);
         for (int i = 0; i < generalInfo.size(); i++) {
             Map<String, String> tempGeneralInfo = generalInfo.get(i);
             List<File> tempFiles = files.get(i);
@@ -43,10 +62,21 @@ public class LobbyView implements NodeViewInterface {
 
             myFlowPane.getChildren().add(tempIcon.getView());
         }
+
+        myVBox.getChildren().add(myFlowPane);
+    }
+
+    private void updateCSS(String newStyleSheet) {
+        myVBox.getStylesheets().clear();
+        myVBox.getStylesheets().add(newStyleSheet);
+    }
+
+    private void updateLanguage(String newLanguage) {
+        myLanguageBundle.setLanguage(newLanguage);
     }
 
     @Override
-    public FlowPane getView() {
-        return myFlowPane;
+    public VBox getView() {
+        return myVBox;
     }
  }
